06f14dfd12d9561960d9df284ee386c8d0658a41
kriviears <kriviears@gmail.com>
2019-02-20 16:43:47 -0500

    Finished updating the statejs to contain all the information for the quiz

--- state.js
+++ state.js
@@ -11,15 +11,15 @@ const STATE = {
   message: [
     'Welcome to the Star trek quiz! Are you ready for some gatekeeping?',
     'Question 1: What do the native people of the planet orbited by the space station \'Deep Space 9\' refer to the captian of that space station as?',
-    'Question 2:',
-    'Question 3:',
-    'Question 4:',
-    'Question 5:',
-    'Question 6:',
-    'Question 7:',
-    'Question 8:',
-    'Question 9:',
-    'Question 10:',
+    'Question 2: What is the color uniform worn by medical/science officers?',
+    'Question 3: (freebie)Which is the worst star trek series?',
+    'Question 4: Which postings did Miles O\'Brien serve on?',
+    'Question 5: Which crew members use the powers given to them by the being Q?',
+    'Question 6: The all powerful being Q appears in how many star trek series?',
+    'Question 7: In the parallel universe first seen in The Orginal Series, what is the name of the Star Fleet equivalent?',
+    'Question 8: Which Star Trek captain has an artificial heart?',
+    'Question 9: Which character was genetically enhanced?',
+    'Question 10: What was the warrior race primarily used by The Dominion?',
     'Restart?'
   ],
   answers: [
@@ -36,52 +36,58 @@ const STATE = {
       {a: 'Emissary', choice: true}
     ],
     [
-      {a: 'Overlord', choice: false},
-      {a: 'Ambassador', choice: false},
-      {a: 'Mesiah', choice: false},
-      {a: 'Emissary', choice: true}
+      {a: 'Red', choice: false},
+      {a: 'Blue', choice: true},
+      {a: 'Gold', choice: false},
+      {a: 'Green', choice: false}
     ],
     [
-      {a: 'Overlord', choice: false},
-      {a: 'Ambassador', choice: false},
-      {a: 'Mesiah', choice: false},
-      {a: 'Emissary', choice: true}
+      {a: 'Star Trek: Voyager', choice: true},
+      {a: 'Star Trek: Voyager', choice: true},
+      {a: 'Star Trek: Voyager', choice: true},
+      {a: 'Star Trek: Voyager', choice: true}
     ],
     [
-      {a: 'Overlord', choice: false},
-      {a: 'Ambassador', choice: false},
-      {a: 'Mesiah', choice: false},
-      {a: 'Emissary', choice: true}
+      {a: 'Enterprise-C', choice: false},
+      {a: 'Enterprise-D', choice: true},
+      {a: 'USS Deviant', choice: false},
+      {a: 'USS Roundabout', choice: false}
     ],
     [
-      {a: 'Overlord', choice: false},
-      {a: 'Ambassador', choice: false},
-      {a: 'Mesiah', choice: false},
-      {a: 'Emissary', choice: true}
+      {a: 'Captian Jean-Luc Picard', choice: false},
+      {a: '1st officer William Riker', choice: true},
+      {a: 'Wesley Crusher', choice: true},
+      {a: 'Ship counselor Deanna Troi', choice: false}
     ],
     [
-      {a: 'Overlord', choice: false},
-      {a: 'Ambassador', choice: false},
-      {a: 'Mesiah', choice: false},
-      {a: 'Emissary', choice: true}
+      {a: '1', choice: false},
+      {a: '2', choice: false},
+      {a: '3', choice: true},
+      {a: '4', choice: false}
     ],
     [
-      {a: 'Overlord', choice: false},
-      {a: 'Ambassador', choice: false},
-      {a: 'Mesiah', choice: false},
-      {a: 'Emissary', choice: true}
+      {a: 'Space Force', choice: false},
+      {a: 'The Dominion', choice: false},
+      {a: 'Terren Empire', choice: true},
+      {a: 'Legion of the Chosen', choice: false}
     ],
     [
-      {a: 'Overlord', choice: false},
-      {a: 'Ambassador', choice: false},
-      {a: 'Mesiah', choice: false},
-      {a: 'Emissary', choice: true}
+      {a: 'Kathryn Janeway', choice: false},
+      {a: 'Jonathan Archer', choice: false},
+      {a: 'Jean-Luc Picard', choice: true},
+      {a: 'Benjamin Sisko', choice: false}
     ],
     [
-      {a: 'Overlord', choice: false},
-      {a: 'Ambassador', choice: false},
-      {a: 'Mesiah', choice: false},
-      {a: 'Emissary', choice: true}
+      {a: 'Julian Bashir', choice: true},
+      {a: 'Odo', choice: false},
+      {a: 'Data', choice: false},
+      {a: 'Spock', choice: false}
+    ],
+    [
+      {a: 'Klingons', choice: false},
+      {a: 'Jem\'Hadar', choice: true},
+      {a: 'Cardassians', choice: false},
+      {a: 'Vorta', choice: false}
     ],
     [
       {a: 'Restart quiz', choice: 'start'},
